0987bf736ab89129ac61ca98e2b6fcc8f5c34ab4
Gulot13 <thejusok@gmail.com>
2018-10-25 13:41:47 +0200

    premier up

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>WAG</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<link rel="stylesheet" type="text/css" href="css/polices.css">
+		<link rel="stylesheet"href="https://www.w3schools.com/w3css/4/w3.css">
+	</head>
+
+	<body>
+		<header>
+				<div class="topnav"> <!--Partie haute du header-->
+					<div class="navleft"> <!-- Partie englobant logo + nav principal du header-->
+						<div class="logo">
+							<img src="images/logo.png">
+						</div>
+						<nav class="headernav">
+							<a target="_blank" href="https://discord.gg/Dm7cwsD">INVITATION</a>
+							<a href="jeux.php">JEUX</a>
+							<a href="recrutement.php">RECRUTEMENT</a>
+							<a href="communaute.php">COMMUNAUTÉ</a>
+							<a href="aide.php">AIDE</a>
+							<a href="boutique.php">BOUTIQUE</a>
+						</nav>
+					</div>
+				</div>
+		</header>
+		<div class="principal"> <!--Partie basse du header-->
+				<h1 class="slogan">BIENVENUE SUR WAG</h1>
+		</div>
+		<section class="section1">
+			<article>
+				<nav class="sectionnav">
+					<a href="actualites.php">ACTUALITÉS</a>
+					<a href="creations.php">CRÉATIONS</a>
+					<a href="communautes.php">COMMUNAUTÉS</a>
+				</nav>
+			</article>
+			<section class="section">
+				<div class="info">
+					
+				</div>
+				<div class="articles">
+					
+				</div>
+			</section>
+		</section>
+	</body>
+
+	<footer>
+		
+	</footer>
+</html>
